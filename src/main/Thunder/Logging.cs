12720e643b0346dd651f6267905af59fba0970f2
Adriano Caldeira <adriano@integgro.com.br>
2012-11-03 06:33:58 -0200

    log

---
+++ src/main/Thunder/Logging.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+
+namespace Thunder
+{
+    /// <summary>
+    /// Logging
+    /// </summary>
+    public static class Logging
+    {
+        /// <summary>
+        /// Write in Application logger configuration of log4net
+        /// </summary>
+        public static ILog Write { get; private set; }
+
+        /// <summary>
+        /// Initialize new instance of <see cref="Logging"/>.
+        /// </summary>
+        static Logging()
+        {
+            Write = LogManager.GetLogger("Application");
+        }
+    }
+}
