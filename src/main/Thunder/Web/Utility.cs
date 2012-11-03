7c0adc433199886a08c93d9f53326b6c0b8d153f
Adriano Caldeira <adriano@integgro.com.br>
2012-11-03 06:43:19 -0200

    converte caminho relativo para absolute a partir do contexto web

---
+++ src/main/Thunder/Web/Utility.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+
+namespace Thunder.Web
+{
+    /// <summary>
+    /// Utility
+    /// </summary>
+    public static class Utility
+    {
+        /// <summary>
+        /// Converts a virtual path to an application absolute path using the <paramref name="context"/>.
+        /// </summary>
+        /// <param name="virtualPath">Virtual path</param>
+        /// <param name="context"><see cref="HttpContextBase"/></param>
+        /// <returns>Absolute path</returns>
+        public static string ToAbsolute(string virtualPath, HttpContextBase context)
+        {
+            return VirtualPathUtility.ToAbsolute(virtualPath, context.Request.ApplicationPath);
+        }
+    }
+}
