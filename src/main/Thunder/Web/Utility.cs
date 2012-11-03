4944efaeb49881eb7bfba4fc6e71e896b627bc0d
Adriano Caldeira <adriano@integgro.com.br>
2012-11-03 06:54:58 -0200

    convert uma url relativa ou absolute de uma aplicacao para um endereo web absolute

--- src/main/Thunder/Web/Utility.cs
+++ src/main/Thunder/Web/Utility.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 
 namespace Thunder.Web
 {
@@ -16,6 +17,25 @@ namespace Thunder.Web
         public static string ToAbsolute(string virtualPath, HttpContextBase context)
         {
             return VirtualPathUtility.ToAbsolute(virtualPath, context.Request.ApplicationPath);
+        }
+
+        /// <summary>
+        /// Converts <paramref name="url"/> to an http absolute address 
+        /// </summary>
+        /// <param name="url">Url</param>
+        /// <param name="request"><see cref="HttpRequestBase"/></param>
+        /// <returns><see cref="Uri"/></returns>
+        public static Uri ToAbsolute(string url, HttpRequestBase request)
+        {
+            var uri = new Uri(url, UriKind.RelativeOrAbsolute);
+
+            if (uri.IsAbsoluteUri)
+                return uri;
+
+            if (request.Url != null)
+                url = String.Format("{0}{1}", request.Url.GetLeftPart(UriPartial.Authority), url);
+
+            return new Uri(url);
         }
     }
 }
