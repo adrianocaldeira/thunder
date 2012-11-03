68440a8ebaa7d8b6ff62d31fc63c360b43c9d5a7
Adriano Caldeira <adriano@integgro.com.br>
2012-11-03 06:29:30 -0200

    extensao de verificacao de ano bisexto de uma data

---
+++ src/main/Thunder/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Thunder
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Check year is bisixth
+        /// </summary>
+        /// <param name="dateTime">Date</param>
+        /// <returns>Year bisixth</returns>
+        public static bool IsYearBisixth(this DateTime dateTime)
+        {
+            return (dateTime.Year%4 == 0 && (dateTime.Year%400 == 0 || dateTime.Year%100 != 0));
+        }
+    }
+}
