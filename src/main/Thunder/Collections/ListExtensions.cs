f2a153733309216ce9cbc9654ca1814b54bd1633
Adriano Caldeira <adriano@integgro.com.br>
2012-11-03 06:04:32 -0200

    atualizacao de dependencia e extencoes de lista

--- src/main/Thunder/Collections/ListExtensions.cs
+++ src/main/Thunder/Collections/ListExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Thunder.Collections
 {
@@ -49,6 +50,77 @@ namespace Thunder.Collections
         public static int Index<T>(this IList<T> list, T @object)
         {
             return list.IndexOf(@object);
+        }
+
+        /// <summary>
+        /// Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"><see cref="T:System.Collections.Generic.ICollection`1"/></param>
+        /// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
+        /// <param name="checkContain">Check if item contains in collection</param>
+        public static void Add<T>(this ICollection<T> source, T item, bool checkContain)
+        {
+            if (checkContain)
+            {
+                if (!source.Contains(item))
+                {
+                    source.Add(item);
+                }
+            }
+            else
+                source.Add(item);
+        }
+
+        /// <summary>
+        /// Adds an items to the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"><see cref="T:System.Collections.Generic.ICollection`1"/></param>
+        /// <param name="items">The objects to add to the <see cref="T:System.Collections.Generic.IList`1"/>.</param>
+        /// <param name="checkContain">Check if item contains in collection</param>
+        public static void Add<T>(this ICollection<T> source, IList<T> items, bool checkContain)
+        {
+            if (checkContain)
+            {
+                foreach (var item in items.Where(item => !source.Contains(item)))
+                {
+                    source.Add(item);
+                }
+
+            }
+            else
+            {
+                foreach (var item in items)
+                {
+                    source.Add(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds an items to the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"><see cref="T:System.Collections.Generic.ICollection`1"/></param>
+        /// <param name="items">The objects to add to the <see cref="T:System.Collections.Generic.IList`1"/>.</param>
+        public static void Add<T>(this ICollection<T> source, IList<T> items)
+        {
+            source.Add(items, true);
+        }
+
+        /// <summary>
+        /// Remove an items from <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"><see cref="T:System.Collections.Generic.ICollection`1"/></param>
+        /// <param name="items"><see cref="T:System.Collections.Generic.IList`1"/></param>
+        public static void Remove<T>(this ICollection<T> source, IList<T> items)
+        {
+            foreach (var item in items)
+            {
+                source.Remove(item);
+            }
         }
     }
 }
