ca14416d1d25b5d19f197031d692368bab84ef7d
Adriano Caldeira <adriano@integgro.com.br>
2012-11-03 06:48:49 -0200

    data annotation de enum requerido

---
+++ src/main/Thunder/ComponentModel/DataAnnotations/EnumRequiredAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Thunder.ComponentModel.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+    public class EnumRequiredAttribute : RequiredAttribute
+    {
+        /// <summary>
+        /// Initialize new instance of <see cref="EnumRequiredAttribute"/>.
+        /// </summary>
+        public EnumRequiredAttribute()
+            : this("NotSet")
+        {
+        }
+
+        /// <summary>
+        /// Initialize new instance of <see cref="EnumRequiredAttribute"/>.
+        /// </summary>
+        /// <param name="notSetValue">Not set value</param>
+        public EnumRequiredAttribute(string notSetValue)
+        {
+            if (String.IsNullOrWhiteSpace(notSetValue))
+            {
+                throw new ArgumentNullException("notSetValue");
+            }
+
+            NotSetValue = notSetValue;
+        }
+
+        /// <summary>
+        /// Get or set not set value
+        /// </summary>
+        public string NotSetValue { get; set; }
+        
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            var notSet = Enum.Parse(value.GetType(), NotSetValue);
+            
+            return !Equals(value, notSet);
+        }
+    }
+}
