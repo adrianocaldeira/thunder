026c61686c3abbbc716f690f2d834832b9f2bdaf
Adriano Caldeira <adriano@integgro.com.br>
2012-11-04 08:04:42 -0200

    validacao de boolean

---
+++ src/main/Thunder/ComponentModel/DataAnnotations/BooleanRequiredAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Thunder.ComponentModel.DataAnnotations
+{
+    ///<summary>
+    /// Boolean validator
+    ///</summary>
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+    public class BooleanRequiredAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Valid object
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            if(value is bool)
+            {
+                return true;
+            }
+
+            bool result;
+
+            return bool.TryParse(value.ToString(), out result);
+        }
+    }
+}
