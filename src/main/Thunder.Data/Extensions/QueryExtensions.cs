047e5b9924e8f090f42f619da0f6857d11d256e8
Adriano Caldeira <adriano@integgro.com.br>
2013-07-28 09:30:26 -0300

    Refatoração de Query

---
+++ src/main/Thunder.Data/Extensions/QueryExtensions.cs
@@ -0,0 +1,75 @@
+﻿using NHibernate;
+using Thunder.Collections;
+using Thunder.Collections.Extensions;
+using Thunder.Model;
+
+namespace Thunder.Data.Extensions
+{
+    /// <summary>
+    /// Query extensions
+    /// </summary>
+    public static class QueryExtensions
+    {
+        /// <summary>
+        /// Paging extensions of <see cref="IQuery"/>
+        /// </summary>
+        /// <typeparam name="T">{T}</typeparam>
+        /// <param name="query"><see cref="IQuery"/></param>
+        /// <param name="currentPage">Current page</param>
+        /// <param name="pageSize">Page size</param>
+        /// <returns><see cref="IPaging{T}"/></returns>
+        public static IPaging<T> Paging<T>(this IQuery query, int currentPage, int pageSize)
+        {
+            return query.Paging<T>(currentPage, pageSize, null);
+        }
+
+        /// <summary>
+        /// Paging extensions of <see cref="IQuery"/> width <see cref="Filter"/>
+        /// </summary>
+        /// <param name="query"><see cref="IQuery"/></param>
+        /// <param name="filter"><see cref="Filter"/></param>
+        /// <typeparam name="T">{T}</typeparam>
+        /// <returns><see cref="IPagingFilter{T}"/></returns>
+        public static IPagingFilter<T> Paging<T>(this IQuery query, Filter filter)
+        {
+            return query.Paging<T>(filter, null);
+        }
+
+        /// <summary>
+        /// Paging extensions of <see cref="IQuery"/>
+        /// </summary>
+        /// <typeparam name="T">{T}</typeparam>
+        /// <param name="query"><see cref="IQuery"/></param>
+        /// <param name="currentPage">Current page</param>
+        /// <param name="pageSize">Page size</param>
+        /// <param name="records">Records</param>
+        /// <returns><see cref="IPaging{T}"/></returns>
+        public static IPaging<T> Paging<T>(this IQuery query, int currentPage, int pageSize, long? records)
+        {
+            query.SetFirstResult(currentPage * pageSize);
+            query.SetMaxResults(pageSize);
+
+            return records.HasValue
+                       ? query.List<T>().Paging(currentPage, pageSize, records.Value)
+                       : query.List<T>().Paging(currentPage, pageSize);
+        }
+
+        /// <summary>
+        /// Paging extensions of <see cref="IQuery"/> with <see cref="Filter"/>
+        /// </summary>
+        /// <param name="query"><see cref="IQuery"/></param>
+        /// <param name="filter"><see cref="Filter"/></param>
+        /// <param name="records">Recrods</param>
+        /// <typeparam name="T">{T}</typeparam>
+        /// <returns><see cref="IPagingFilter{T}"/></returns>
+        public static IPagingFilter<T> Paging<T>(this IQuery query, Filter filter, long? records)
+        {
+            query.SetFirstResult(filter.CurrentPage * filter.PageSize);
+            query.SetMaxResults(filter.PageSize);
+
+            return records.HasValue
+                       ? query.List<T>().Paging(filter, records.Value)
+                       : query.List<T>().Paging(filter);
+        }
+    }
+}
