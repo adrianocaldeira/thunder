dcae97e8c2139555a8dde712cbd1003e02a46c8f
Jarbas Brito <jbmsegundo@gmail.com>
2013-08-06 12:56:12 -0300

    added sessionperrequest filter for webapi

---
+++ src/main/Thunder.Web.Mvc/SessionPerRequestWebApiAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http.Filters;
+using Thunder.Data;
+
+namespace Thunder.Web.Mvc
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+    public class SessionPerRequestWebApi : ActionFilterAttribute
+    {
+        public string Exclude { get; set; }
+        public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            base.OnActionExecuting(actionContext);
+
+            if (Apply(actionContext.ActionDescriptor.ActionName))
+            {
+
+                SessionManager.Bind();
+
+            }
+
+        }
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            base.OnActionExecuted(actionExecutedContext);
+            if (Apply(actionExecutedContext.ActionContext.ActionDescriptor.ActionName))
+            {
+
+                SessionManager.Unbind();
+            }
+        }
+
+        private bool Apply(string action)
+        {
+            return string.IsNullOrEmpty(Exclude) || Exclude.Split(',').All(item => !item.ToLower().Equals(action.ToLower()));
+        }
+    }
+}
