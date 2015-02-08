e04a9d2853f1df71a3d19a2ce8b84e8adf7fd8ce
Adriano Caldeira <adriano@integgro.com.br>
2015-02-08 18:42:09 -0200

    melhoria #7

--- src/main/Thunder/Extensions/StringExtensions.cs
+++ src/main/Thunder/Extensions/StringExtensions.cs
@@ -448,5 +448,26 @@ namespace Thunder.Extensions
 
             return BitConverter.ToInt64(provider.ComputeHash(Encoding.Unicode.GetBytes(s)), 0);
         }
+
+        /// <summary>
+        ///  Checks if url is valid
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static bool IsValidUrl(this string url)
+        {
+            const string regex = "^(https?://)"
+                                 + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //user@
+                                 + @"(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP- 199.194.52.184
+                                 + "|" // allows either IP or domain
+                                 + @"([0-9a-z_!~*'()-]+\.)*" // tertiary domain(s)- www.
+                                 + @"([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]" // second level domain
+                                 + @"(\.[a-z]{2,6})?)" // first level domain- .com or .museum is optional
+                                 + "(:[0-9]{1,5})?" // port number- :80
+                                 + "((/?)|" // a slash isn't required if there is no file name
+                                 + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+
+            return new Regex(regex).IsMatch(url);
+        }
     }
 }
