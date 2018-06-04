6de30393d4b9ef5e3b7a3a3cd755831c0e853389
Adriano Caldeira <adcaldeira@outlook.com>
2018-06-04 10:19:47 -0300

    Implementação OnPreUpdateAsync e OnPreInsertAsync

--- src/main/Thunder.NHibernate/Pattern/CreatedAndUpdatedPropertyEventListener.cs
+++ src/main/Thunder.NHibernate/Pattern/CreatedAndUpdatedPropertyEventListener.cs
@@ -8,22 +8,48 @@ using Thunder.Data;
 namespace Thunder.NHibernate.Pattern
 {
     /// <summary>
-    /// Created and updated property event listener
+    ///     Created and updated property event listener
     /// </summary>
     [Serializable]
     public class CreatedAndUpdatedPropertyEventListener : IPreUpdateEventListener, IPreInsertEventListener
     {
+        private static void Set(IEntityPersister persister, object[] state, string propertyName, object value)
+        {
+            var index = Array.IndexOf(persister.PropertyNames, propertyName);
+
+            if (index == -1)
+                return;
+
+            state[index] = value;
+        }
+
         #region Implementation of IPreUpdateEventListener
 
+        /// <summary> Return true if the operation should be vetoed</summary>
+        /// <param name="event"></param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the work</param>
         public Task<bool> OnPreUpdateAsync(PreUpdateEvent @event, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var entity = @event.Entity as ICreatedAndUpdatedProperty;
+
+            if (entity == null)
+                return Task.FromResult(false);
+
+            var time = DateTime.Now;
+
+            Set(@event.Persister, @event.State, "Updated", time);
+
+            entity.Updated = time;
+
+            if (entity.Created == DateTime.MinValue) entity.Created = time;
+
+            return Task.FromResult(false);
         }
 
         /// <summary>
-        /// Return true if the operation should be vetoed
+        ///     Return true if the operation should be vetoed
         /// </summary>
-        /// <param name="event"/>
+        /// <param name="event" />
         public bool OnPreUpdate(PreUpdateEvent @event)
         {
             var entity = @event.Entity as ICreatedAndUpdatedProperty;
@@ -32,13 +58,13 @@ namespace Thunder.NHibernate.Pattern
                 return false;
 
             var time = DateTime.Now;
-            
+
             Set(@event.Persister, @event.State, "Updated", time);
 
             entity.Updated = time;
 
             if (entity.Created == DateTime.MinValue) entity.Created = time;
-            
+
             return false;
         }
 
@@ -46,15 +72,31 @@ namespace Thunder.NHibernate.Pattern
 
         #region Implementation of IPreInsertEventListener
 
+        /// <summary> Return true if the operation should be vetoed</summary>
+        /// <param name="event"></param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the work</param>
         public Task<bool> OnPreInsertAsync(PreInsertEvent @event, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var entity = @event.Entity as ICreatedAndUpdatedProperty;
+
+            if (entity == null)
+                return Task.FromResult(false);
+
+            var time = entity.Created == DateTime.MinValue ? DateTime.Now : entity.Created;
+
+            Set(@event.Persister, @event.State, "Updated", time);
+            Set(@event.Persister, @event.State, "Created", time);
+
+            entity.Updated = time;
+            entity.Created = time;
+
+            return Task.FromResult(false);
         }
 
         /// <summary>
-        /// Return true if the operation should be vetoed
+        ///     Return true if the operation should be vetoed
         /// </summary>
-        /// <param name="event"/>
+        /// <param name="event" />
         public bool OnPreInsert(PreInsertEvent @event)
         {
             var entity = @event.Entity as ICreatedAndUpdatedProperty;
@@ -62,8 +104,7 @@ namespace Thunder.NHibernate.Pattern
             if (entity == null)
                 return false;
 
-            var time = (entity.Created == DateTime.MinValue ?
-                DateTime.Now : entity.Created);
+            var time = entity.Created == DateTime.MinValue ? DateTime.Now : entity.Created;
 
             Set(@event.Persister, @event.State, "Updated", time);
             Set(@event.Persister, @event.State, "Created", time);
@@ -75,15 +116,5 @@ namespace Thunder.NHibernate.Pattern
         }
 
         #endregion
-
-        private static void Set(IEntityPersister persister, object[] state, string propertyName, object value)
-        {
-            var index = Array.IndexOf(persister.PropertyNames, propertyName);
-            
-            if (index == -1)
-                return;
-
-            state[index] = value;
-        }
     }
 }
