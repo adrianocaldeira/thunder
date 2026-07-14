fa19deab397ab1979d743d2e4b8a29d8f966e443
Adriano Caldeira <adriano@msatec.com.br>
2026-07-14 10:57:54 -0300

    fix: corrige codificação do texto Não em BooleanExtensions

---
+++ src/test/Thunder/Extensions/BooleanExtensionsTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Thunder.Extensions
+{
+    [TestFixture]
+    public class BooleanExtensionsTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual("Não", false.Text());
+            Assert.AreEqual("Sim", true.Text());
+        }
+    }
+}
